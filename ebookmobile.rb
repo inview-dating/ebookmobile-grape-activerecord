00c00ae3dbcc67cb19000f66ca75e7a028e904d0
Dana Scheider <dana.scheider@gmail.com>
2016-07-17 15:25:43 -0700

    Make API work

--- ebookmobile.rb
+++ ebookmobile.rb
@@ -7,5 +7,22 @@ Grape::ActiveRecord.configure_from_file! "config/database.yml"
 
 module Ebookmobile
   class API < Grape::API
+
+    resource :authors do 
+      desc "Return a list of authors"
+      get do 
+        Author.all.to_json
+      end
+
+      desc "Add a new author to the database"
+      post do 
+        params = JSON.parse(request.body.read, symbolize_names: true).fetch(:author)
+
+        @author = Author.create!(params)
+
+        status 200
+        @author.to_json
+      end
+    end
   end
 end
